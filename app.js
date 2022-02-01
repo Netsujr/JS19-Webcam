0245eab6bc9c4c28543158206cb48d2d1ab95b2f
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-01 10:46:02 +0900

    green screen func

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height); // array of pixels (rgba)
     // the 0, 0 is where it should start from. then width and height is destination
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
     ctx.putImageData(pixels, 0, 0);
+    // ctx.globalAlpha = 0.1;
+
+
     // 1. Take pixels data || 2. change the pixels data || 3. put it back to the canvas
   }, 16);
 }
@@ -45,11 +49,9 @@ function takePhoto() {
   strip.insertBefore(link, strip.firstChild());
 }
 
-getVideo();
-video.addEventListener('canplay', paintToCanvas);
 
 function redEffect(pixels) {
-  for(let i = 0; i < pixels.data.length; i += 4) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i + 0] = pixels.data[i + 0] + 200; // red
     pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
@@ -57,5 +59,42 @@ function redEffect(pixels) {
   return pixels;
 }
 
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 100] = pixels.data[i + 1];
+    pixels.data[i - 150] = pixels.data[i + 2];
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  [...document.querySelectorAll('rgb input')].forEach((input) => {
+    levels.[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+}
+
 // localhost works but camera does not work
 // cannot 'getUserMedia' over synched network
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
