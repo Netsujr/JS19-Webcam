e0d9c23571c2f33bb5b73aa6717b0a7728573714
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-01 10:11:18 +0900

    capture photo function

--- app.js
+++ app.js
@@ -16,15 +16,26 @@ function getVideo() {
     });
 }
 
-function paintToCanvas () {
+function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
   // canvas must be same size as video
   canvas.width = width;
   canvas.height = height;
 
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // the 0, 0 is where it should start from. then width and height is destination
+  }, 16);
 }
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+}
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
 
 // localhost works but camera does not work
 // cannot 'getUserMedia' over synched network
- getVideo();
